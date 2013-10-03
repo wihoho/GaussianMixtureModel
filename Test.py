3f5017acaaf478c6e6feeb6c5d7abb52f2644e65
wihoho <wihoho@gmail.com>
2013-10-03 23:03:32 +0800

    add some test cases

--- Test.py
+++ Test.py
@@ -1,5 +1,8 @@
 import Utility as util
 
 globalGMM = util.loadObject("/Users/GongLi/PycharmProjects/GaussianMixtureModel/ClusterSample50/GlobalGaussianMixtureModel.pkl")
+miniBatch = util.loadObject("/Users/GongLi/PycharmProjects/GaussianMixtureModel/ClusterSample50/miniBatchCluster.pkl")
+
+fullCovarianceGMM = util.loadObject("/Users/GongLi/PycharmProjects/GaussianMixtureModel/ClusterSample50/FullCovariance_GlobalGaussianMixtureModel.pkl")
 print "Yes"
 
